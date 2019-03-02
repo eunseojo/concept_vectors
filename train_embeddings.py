1560c8d53e1a4b6d4b49c774c9bde6beb86168b6
Eun Seo Jo <eunseojo@gmail.com>
2019-03-02 14:07:42 -0800

    option to save as text

--- train_embeddings.py
+++ train_embeddings.py
@@ -5,5 +5,5 @@ text = textread.read()
 textinput = text.split()
 model = Word2Vec([textinput], size=300, window=3, min_count=3, workers=4)
 model.save("/Users/eunseo/concept_vectors/model1")
-
+#model.wv.save_word2vec_format("/Users/eunseo/concept_vectors/model1text"))
 
